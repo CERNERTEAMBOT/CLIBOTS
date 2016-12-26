dfb15b16c6ab5bf01a2166dad6273fa647b039fb
telecerner <amircerner@gmail.com>
2016-12-26 19:10:55 +0330

    Update CerNer.lua

--- plugins/CerNer.lua
+++ plugins/CerNer.lua
@@ -8,7 +8,7 @@ local function run(msg, matches)
   if matches[1] == 'Team' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./data/me/sudo.webp", ok_cb, false)
-      return "<i>CerNer Team</i>\n<b>Developer</b> ~ @mrcliapi\n<i>admin</i> ~ @IR_virtual\n<b>bots</b>=\n<i>1 Nod32 F1 ~ username</i> @NOD32TG\n<i>2 Nod32 F2 ~ username</i> @NOD32F2\n<b>Channel</b> @CerNerTeam"
+      return "<i>CerNer Team</i>\n<b>Developer</b> ~ @mrcliapi\n<i>admin</i> ~ @IR_virtual\n<b>bots</b>=\n<i>1 CerNer F1 ~ username</i> @NerNerTG\n<i>2 CerNer F2 ~ username</i> @CerNerplus\n<b>Channel</b> @CerNerTeam"
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./data/me/admin.webp", ok_cb, false)
       return "<i>YoU Admin</i>"
@@ -29,8 +29,6 @@ return {
   patterns = {
     "^[#!/]([Tt]eam)$",
     "^([Tt]eam)$"
-     "^([Nn]od32)$"
-      "^[#!/]([Nn]od32)$"
     },
   run = run
 }
